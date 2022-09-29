281d175f06351e3c1b5d170308aa43192bca4902
itmm-2213-a <itmm-2213-a@E.local>
2022-09-29 09:41:00 +0300

    add ficha

--- TC_continuation/Arrays_Easy/arrays_task.c
+++ TC_continuation/Arrays_Easy/arrays_task.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
 int main() {
-	printf("Hello!\n");
+	int a;
+	printf("Hello!\nInput u number: ");
+
+	scanf_s("%d", &a);
+
+	printf("Thanx\n");
 	system("pause");
 	return 0;
 }
